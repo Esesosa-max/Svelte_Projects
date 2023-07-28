d6525c68fa0fbbca6d87ac38c465f4de573ead09
Idukpaye alex <75207794+Esesosa-max@users.noreply.github.com>
2023-07-28 15:14:18 +0100

    Add files via upload

--- src/routes/grid.svelte
+++ src/routes/grid.svelte
@@ -7,22 +7,3 @@
 	import photo6 from '$lib/photo6.png';
 </script>
 
-<div class=" m-auto grid grid-cols-3 justify-stretch gap-3 mt-3 " id="grid">
-	<img src={photo1} class="col-span-1 w-full h-full rounded-lg" alt="" />
-	<img src={photo2} class="col-span-2 w-full h-full  rounded-lg " />
-	<img src={photo3} class="col-span-2 w-full h-full  rounded-lg" />
-	<img src={photo_4} class="col-span-1 w-full h-full row-span-2  rounded-lg" />
-	<img src={photo5} alt="" class="col-span-1 w-full h-full row-start-3  rounded-lg" />
-	<img src={photo6} alt="" class="col-span-1 w-full h-full  rounded-lg" />
-</div>
-
-<style>
-	#grid {
-		max-width: 900px;
-	}
-	@media (max-width: 900px) {
-		#grid {
-			width: 90%;
-		}
-	}
-</style>
