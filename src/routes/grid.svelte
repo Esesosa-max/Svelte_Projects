7877356da724417e01b96a3e8aabe1d0bcc300f0
Idukpaye alex <75207794+Esesosa-max@users.noreply.github.com>
2023-07-31 14:34:48 +0100

    Add files via upload

--- src/routes/grid.svelte
+++ src/routes/grid.svelte
@@ -1,28 +1,4 @@
 <script>
-	import photo1 from '$lib/photo1.png';
-	import photo2 from '$lib/photo2.png';
-	import photo3 from '$lib/photo3.png';
-	import photo_4 from '$lib/photo_4.png';
-	import photo5 from '$lib/photo5.png';
-	import photo6 from '$lib/photo6.png';
+	;
 </script>
 
-<div class=" m-auto grid grid-cols-3 justify-stretch gap-3 mt-3 " id="grid">
-	<img src={photo1} class="col-span-1 w-full h-full rounded-lg" alt="" />
-	<img src={photo2} class="col-span-2 w-full h-full  rounded-lg " />
-	<img src={photo3} class="col-span-2 w-full h-full  rounded-lg" />
-	<img src={photo_4} class="col-span-1 w-full h-full row-span-2  rounded-lg" />
-	<img src={photo5} alt="" class="col-span-1 w-full h-full row-start-3  rounded-lg" />
-	<img src={photo6} alt="" class="col-span-1 w-full h-full  rounded-lg" />
-</div>
-
-<style>
-	#grid {
-		max-width: 900px;
-	}
-	@media (max-width: 900px) {
-		#grid {
-			width: 90%;
-		}
-	}
-</style>
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,27 +1,17 @@
 <script>
-	import profilePhoto from '$lib/profilePhoto.png';
+	import logo from '$lib/logo.svg';
+	import dataset from '$lib/stays.json';
+	import stringSimilarity from 'string-similarity';
+	
 </script>
 
-<header class="flex justify-center mt-10">
-	<div class="">
-		<div class="flex flex-col justify-center">
-			<img src={profilePhoto} alt="" class=" m-auto text-center w-[160px] rounded-lg" />
-			<h4 class="text-center text-xl text-black font-bold">Rodney Cotton</h4>
-			<p class="text-center">Heiskinki Finland</p>
-		</div>
-		<div class="tab-container mt-3 flex justify-between w-[300px] ">
-			<div class="tab bg-slate-300 px-4 py-1 rounded-lg">
-				<h4>100</h4>
-				<p>Posts</p>
-			</div>
-			<div class="tab bg-slate-300 px-2 py-1 rounded-lg">
-				<h4>2,243</h4>
-				<p>Followers</p>
-			</div>
-			<div class="tab bg-slate-300 px-2 py-1 rounded-lg">
-				<h4>1432</h4>
-				<p>Following</p>
-			</div>
-		</div>
-	</div>
+<header class="">
+
 </header>
+<Grid/>
+
+<style>
+	.none {
+		display: none;
+	}
+</style>
